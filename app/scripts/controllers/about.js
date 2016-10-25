ae449587737dab2abc880f74aa44eef80ab80e29
gojicmiroslav <miroslavy2k@gmail.com>
2016-10-25 23:58:24 +0200

    Init commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mgMessageClientApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mgMessageClientApp
+ */
+angular.module('mgMessageClientApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
