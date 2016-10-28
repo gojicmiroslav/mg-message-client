47a70c6c8c6101f6f205325b69ed598340152227
gojicmiroslav <miroslavy2k@gmail.com>
2016-10-28 22:07:44 +0200

    Send message with angular

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,13 +9,19 @@
  */
 angular.module('mgMessageClientApp')
   	.controller('MainCtrl', ['$http', function ($http) {
-    
+
   		this.postMessage = function(){
-  			console.log('Post');
+  			$http.post('http://localhost:5000/api/message', { msg: this.message });
+        $http.post('https://mgmessage-server.herokuapp.com/api/message', { msg: this.message });
+    		//this.getMessages();
   		};
 
   		this.getMessages = function(){
-  			
+  			//vm -view-model
+    		var vm = this;
+    		$http.get('http://localhost:5000/api/messages').then(function(result){
+    			vm.messages = result.data;
+    		});
     	};
 
   	}]);
