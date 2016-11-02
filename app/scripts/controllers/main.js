036c8f92d88d24934b78056f917c72f14210e1af
gojicmiroslav <miroslavy2k@gmail.com>
2016-11-02 21:56:28 +0100

    Heroku fix #7

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,7 +11,7 @@ angular.module('mgMessageClientApp')
   	.controller('MainCtrl', ['$http', function ($http) {
 
   		this.postMessage = function(){
-  			$http.post('http://localhost:5000/api/message', { msg: this.message });
+  			//$http.post('http://localhost:5000/api/message', { msg: this.message });
         $http.post('https://mgmessage-server.herokuapp.com/api/message', { msg: this.message });
     		//this.getMessages();
   		};
